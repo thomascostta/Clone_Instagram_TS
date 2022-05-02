494d4bcee551fe6e7b29754b7c26dae75a5ea770
thomascostta <thomas.jeffcosta@gmail.com>
2022-05-02 11:17:32 -0300

    settings

--- src/components/molecules/PostAbout/index.tsx
+++ src/components/molecules/PostAbout/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, ImageProps } from 'react-native';
 
-import { styles } from "../../../pages/Feed/styles";
+import { styles } from "./styles";
 
 import { PostLiked } from '../../atoms/PostLiked';
 import { PostLikedAvatar } from '../../atoms/PostLikedAvatar';
